1d09e4570d44b17429163f127330ccbad98ec64d
itsudayy <lemphardjohn@gmail.com>
2026-06-07 00:45:39 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,31 +1,39 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const Product = require("./models/product.model.js");
-const productRoute = require("./routes/product.route.js");
+const studentRoute = require("./routes/student.route.js");
 const app = express();
 
-
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-
-app.use("/api/products", productRoute);
+app.use(express.urlencoded({ extended: false }));
 
 
-mongoose.connect("mongodb+srv://admin:shafin@backenddb.hldvorr.mongodb.net/?appName=BackendDB")
+app.use("/", studentRoute);
+
+
+app.get('/home', (req, res) => {
+    res.send('Welcome to node js server');
+});
+
+app.get('/contact', (req, res) => {
+    res.send('Contact Page');
+});
+
+app.get('/services', (req, res) => {
+    res.send('Services Page');
+});
+
+
+const mongoURI = "mongodb+srv://admin:shafin@studentdb.hldvorr.mongodb.net/studentDB?appName=BackendDB";
+
+mongoose.connect(mongoURI)
     .then(() => {
-        console.log('Connected to MongoDB');
+        console.log('MongoDB Connected Successfully');
     })
     .catch((error) => {
         console.error('Error connecting to MongoDB:', error);
     });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+// Running on port 4000
+app.listen(4000, () => {
+    console.log('Server is running on port 4000');
 });
-
-
-app.get('/', (req, res) => {
-    res.send('Hello from crud api');
-});
-
-
